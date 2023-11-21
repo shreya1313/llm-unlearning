324c50506dc83a0aee3aaee07551d37287496184
Saaketh Koundinya Gundavarapu <sg7729@log-2.hpc.nyu.edu>
2023-11-20 22:04:30 -0500

    baseline

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # machine-unlearning
+
+# Replication
+
+## Dataset - TruthfuQA, PKU-SafeRLHF
+
+## Models
+## https://github.com/kevinyaobytedance/llm_unlearn
+- OPT-1.3B
+- OPT-2.7B
+- Llama 2 (7B)
+
+## Implementation
+
+Tasks:
+
+- Dataset preparation - ?
+
+- Training Unlearning
+
+- Inference 5 examples
+
+- Evalution - From paper
+
+# Novelity
+
+## Dataest
+
+Tasks:
+
+- Find dataset for harmful (check HarmfulQA)
+
+- Find dataset for copyright (Harry potter check)
+
+## Models
+
+- OPT-1.3B
+- OPT-2.7B
+- Llama 2 (7B)
+
+## Implementation
+
+Tasks:
+
+- Dataset preparation 
+
+- Training Unlearning
+
+- Inference 5 examples
+
+- Evalution 
---
+++ scripts/baseline_2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=1
+#SBATCH --time=12:00:00
+#SBATCH --mem=64GB
+#SBATCH --gres=gpu:v100:2
+#SBATCH --job-name=unlearning
+#SBATCH --output=baseline_2.out
+
+module purge
+
+singularity exec --nv \
+	    --overlay /scratch/sg7729/my_env/overlay-15GB-500K.ext3:ro \
+	    /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif\
+	    /bin/bash -c "source /ext3/env.sh; python /scratch/sg7729/machine-unlearning/unlearn_harm.py --model_name=facebook/opt-1.3b --model_save_dir=/scratch/sg7729/machine-unlearning/models/opt1.3b_unlearned --log_file=/scratch/sg7729/machine-unlearning/logs/opt-1.3b-unlearn.log"
+
+		
