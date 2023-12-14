abb0f1deea6a16fcbd44984cfe001f2896576fa6
Arushi Arora <aroraarushi1997@gmail.com>
2023-12-13 20:39:05 -0500

    Adding copyright for opt 2.7b and evaluation

---
+++ copyright/opt-2.7b/training_utils.py
@@ -0,0 +1,36 @@
+
+from torch import float32, nn, exp
+
+
+class CastOutputToFloat(nn.Sequential):
+    def forward(self, x): 
+        return super().forward(x).to(float32)
+
+
+def prepare_model(model):
+    for param in model.parameters():
+      param.requires_grad = False  # freeze the model - train adapters later
+      if param.ndim == 1:
+        # cast the small parameters (e.g. layernorm) to fp32 for stability
+        param.data = param.data.to(float32)
+    model.gradient_checkpointing_enable()  # reduce number of stored activations
+    model.enable_input_require_grads()
+    model.lm_head = CastOutputToFloat(model.lm_head)
+    return model
+
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    return f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+
+
+def compute_perplexity(pred):
+    pass
